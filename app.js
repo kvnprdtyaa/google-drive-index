1a0bba37bf7f356235c58419842f9f111fe41573
KvnPrdtyaa <kevinpradityabussines@gmail.com>
2025-02-21 15:53:21 +0700

    update

--- app.js
+++ app.js
@@ -5,18 +5,20 @@ function init() {
         <div id="nav"></div>
     </header>
     <div class="loading" id="spinner" style="display:none;">Loading&#8230;</div>
-    <div id="content" style="padding-top: 20px;"></div>
-    <div class="modal fade" id="SearchModel" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="SearchModelLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                <h5 class="modal-title" id="SearchModelLabel"></h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true"></span>
-                </button>
+    <div>
+        <div id="content" style="padding-top: 20px;"></div>
+        <div class="modal fade" id="SearchModel" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="SearchModelLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                    <h5 class="modal-title" id="SearchModelLabel"></h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true"></span>
+                    </button>
+                </div>
+                <div class="modal-body" id="modal-body-space"></div>
+                <div class="modal-footer" id="modal-body-space-buttons"></div>
             </div>
-            <div class="modal-body" id="modal-body-space"></div>
-            <div class="modal-footer" id="modal-body-space-buttons"></div>
         </div>
     </div>
     <br>
