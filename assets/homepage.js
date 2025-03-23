6a34b4b7bc651a30314efb1361a51dc457f3c88d
kvnprdtyaa <kevinpradityabussines@gmail.com>
2025-03-23 17:12:39 +0700

    Update homepage.js

--- assets/homepage.js
+++ assets/homepage.js
@@ -9,7 +9,7 @@ for (let i = 0; i < len; i++) {
                <stop offset="0" stop-color="#eba600"></stop>
                <stop offset="1" stop-color="#c28200"></stop>
             </linearGradient>
-            <path fill="url(#WQEfvoQAcpQgQgyjQQ4Hqa)" d="M24.414,10.414l-2.536-2.536C21.316,7.316,20.553,7,19.757,7L5,7C3.895,7,3,7.895,3,9l0,30	c0,1.105,0.895,2,2,2l38,0c1.105,0,2-0.895,2-2V13c0-1.105-0.895-2-2-2l-17.172,0C25.298,11,24.789,10.789,24.414,10.414z"></path>
+            <path fill="url(#WQEfvoQAcpQgQgyjQQ4Hqa)" d="M24.414,10.414l-2.536-2.536C21.316,7.316,20.553,7,19.757,7L5,7C3.895,7,3,7.895,3,9l0,30 c0,1.105,0.895,2,2,2l38,0c1.105,0,2-0.895,2-2V13c0-1.105-0.895-2-2-2l-17.172,0C25.298,11,24.789,10.789,24.414,10.414z"></path>
             <linearGradient id="WQEfvoQAcpQgQgyjQQ4Hqb" x1="24" x2="24" y1="10.854" y2="40.983" gradientUnits="userSpaceOnUse">
                <stop offset="0" stop-color="#ffd869"></stop>
                <stop offset="1" stop-color="#fec52b"></stop>
